b0be9d95e057d2b519a1e4f5ee8e3d80e873762b
xuweicheng <qin_liyao@163.com>
2018-11-14 21:08:19 +0800

    less文件

--- src/pages/message/index.vue
+++ src/pages/message/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="message">
         <div>
             消息
         </div>
--- src/pages/personal-center/personal-setting/index.vue
+++ src/pages/personal-center/personal-setting/index.vue
@@ -24,6 +24,4 @@ export default {
     }
 }
 </script>
-<style lang="less" scoped>
-</style>
 
