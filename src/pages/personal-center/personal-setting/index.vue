8a217151cc4769e69a373a9c3fcfee7dbbe0371b
xuweicheng <qin_liyao@163.com>
2018-12-06 15:40:06 +0800

    test

--- src/pages/personal-center/personal-setting/index.vue
+++ src/pages/personal-center/personal-setting/index.vue
@@ -1,47 +1,57 @@
 <template>
-    <div class="personal_setting">
-        <div class="setting_head">
-            <div class="pet_name">
-                <img src="../../../assets/images/headPhone.jpg" alt="">
-                <div>
-                    <div>芝麻凛</div>
-                    <div>1110</div>
-                </div>
-            </div>
-            <svg class="icon" aria-hidden="true">
-                <use xlink:href="#icon-youjiantou-copy"></use>
-            </svg>
-        </div>
+  <div class="personal_setting">
+    <div class="setting_head">
+      <div class="pet_name">
+        <img src="../../../assets/images/headPhone.jpg" alt>
         <div>
-            <x-address :title="title" v-model="value" :list="addressData" placeholder="请选择"></x-address>
+          <div>芝麻凛</div>
+          <div>1110</div>
         </div>
+      </div>
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#icon-youjiantou-copy"></use>
+      </svg>
     </div>
+    <group label-margin-right="4em" label-align="right">
+      <x-address
+        :title="title"
+        v-model="value"
+        :list="addressData"
+        placeholder="请选择"
+        class="ignore"
+      ></x-address>
+    </group>
+    <group>
+      <x-switch :title="$t('Toggle')" v-model="show"></x-switch>
+    </group>
+  </div>
 </template>
 <script>
-import { Group, Picker, XAddress, ChinaAddressV4Data, Value2nameFilter as value2name } from 'vux'
-
+import {
+  Group,
+  Picker,
+  XAddress,
+  ChinaAddressV4Data,
+  Value2nameFilter as value2name
+} from "vux";
 
 export default {
-    components: {
-        Picker,
-        XAddress,
-        Group
-    },
-    name: 'personalSetting',
-    data() {
-        return {
-            title: '地址',
-            value: [],
-            showAddress: false,
-            addressData: ChinaAddressV4Data
-        }
-    },
-    mounted() {
-
-    },
-    methods: {
-
-    }
-}
+  components: {
+    Picker,
+    XAddress,
+    Group
+  },
+  name: "personalSetting",
+  data() {
+    return {
+      title: "地址",
+      value: [],
+      showAddress: false,
+      addressData: ChinaAddressV4Data
+    };
+  },
+  mounted() { },
+  methods: {}
+};
 </script>
 
