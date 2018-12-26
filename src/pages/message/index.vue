6e0b484f8f0698d68bffcd2e5e8a454fcf532b28
xuweicheng <qin_liyao@163.com>
2018-12-26 17:45:37 +0800

    message页

--- src/pages/message/index.vue
+++ src/pages/message/index.vue
@@ -1,21 +1,27 @@
 <template>
-    <div>
-        消息
-        <icon-library></icon-library>
-    </div>
+	<div class="abouts">消息
+		<!-- <icon-library></icon-library> -->
+		<div class="square">
+			<div class="s-top"></div>
+			<div class="s-middle"></div>
+			<div class="s-bottom"></div>
+			<div class="s-half-circle"></div>
+			<div class="s-background"></div>
+		</div>
+		<div class="circle"></div>
+	</div>
 </template>
 <script>
 import iconLibrary from '@/components/icon-library'
 export default {
-    components: {
-        iconLibrary
-    },
-    name: 'community',
-    data() {
-        return {
-
-        }
-    }
+	components: {
+		iconLibrary
+	},
+	name: 'community',
+	data() {
+		return {}
+	}
 }
 </script>
+
 
