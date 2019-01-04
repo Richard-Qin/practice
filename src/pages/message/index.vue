cdc19d6a2687ba94057d7cfc7f88b94348132a5e
xuweicheng <qin_liyao@163.com>
2019-01-04 09:12:58 +0800

    删除测试代码

--- src/pages/message/index.vue
+++ src/pages/message/index.vue
@@ -1,14 +1,6 @@
 <template>
 	<div class="abouts">消息
-		<!-- <icon-library></icon-library> -->
-		<div class="square">
-			<div class="s-top"></div>
-			<div class="s-middle"></div>
-			<div class="s-bottom"></div>
-			<div class="s-half-circle"></div>
-			<div class="s-background"></div>
-		</div>
-		<div class="circle"></div>
+		<icon-library></icon-library>
 	</div>
 </template>
 <script>
