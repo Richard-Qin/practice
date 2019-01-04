66f626b200b7e065dba1e2894efa4ae04adde89b
xuweicheng <qin_liyao@163.com>
2019-01-04 16:58:23 +0800

    vscode格式化配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import routes from './routes'
-// import personalCenter from '@/pages/personal-center/index'
-// import TabBar from '@/components/tabbar'
 
 Vue.use(Router)
 
@@ -11,19 +9,3 @@ let _router = new Router({
 })
 
 export default _router
-
-
-
-// export default new Router({
-//   routes: [{
-//       path: '/personal-center',
-//       name: 'personalCenter',
-//       component: personalCenter
-//     },
-//     {
-//       path: '/components',
-//       name: 'tabBar',
-//       component: TabBar
-//     }
-//   ]
-// })
