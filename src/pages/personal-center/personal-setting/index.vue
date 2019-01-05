2cdd39d0246ab706dcd0df3bddb2d37629fe6078
xuweicheng <qin_liyao@163.com>
2019-01-05 17:53:49 +0800

    删除报错代码

--- src/pages/personal-center/personal-setting/index.vue
+++ src/pages/personal-center/personal-setting/index.vue
@@ -1,82 +1,61 @@
 <template>
-  <div class="personal_setting">
-    <div class="setting_head">
-      <div class="pet_name">
-        <img src="../../../assets/images/headPhone.jpg" alt>
-        <div>
-          <div>芝麻凛</div>
-          <div>1110</div>
-        </div>
-      </div>
-      <svg class="icon" aria-hidden="true">
-        <use xlink:href="#icon-youjiantou-copy"></use>
-      </svg>
-    </div>
-    <group label-margin-right="4em" label-align="right">
-      <x-address
-        :title="title"
-        v-model="value"
-        :list="addressData"
-        placeholder="请选择"
-        class="ignore"
-      ></x-address>
-    </group>
-    <group>
-      <x-switch :title="$t('Toggle')" v-model="show"></x-switch>
-    </group>
-    <div v-transfer-dom>
-      <confirm
-        v-model="show"
-        :title="$t('Confirm deleting the item')"
-        @on-cancel="onCancel"
-        @on-confirm="onConfirm"
-        @on-show="onShow"
-        @on-hide="onHide"
-      >
-        <p style="text-align:center;">{{ $t('Are you sure?') }}</p>
-      </confirm>
-    </div>
-  </div>
+	<div class="personal_setting">
+		<div class="setting_head">
+			<div class="pet_name">
+				<img alt src="../../../assets/images/headPhone.jpg">
+				<div>
+					<div>芝麻凛</div>
+					<div>1110</div>
+				</div>
+			</div>
+			<svg aria-hidden="true" class="icon">
+				<use xlink:href="#icon-youjiantou-copy"></use>
+			</svg>
+		</div>
+		<group label-align="right" label-margin-right="4em">
+			<x-address :list="addressData" :title="title" class="ignore" placeholder="请选择" v-model="value"></x-address>
+		</group>
+	</div>
 </template>
 <script>
 import {
-  Group,
-  Picker,
-  XAddress,
-  ChinaAddressV4Data,
-  Confirm,
-  Value2nameFilter as value2name
-} from "vux";
+	Group,
+	Picker,
+	XAddress,
+	ChinaAddressV4Data,
+	Confirm,
+	Value2nameFilter as value2name
+} from 'vux'
 
 export default {
-  components: {
-    Confirm,
-    Picker,
-    XAddress,
-    Group
-  },
-  name: "personalSetting",
-  data() {
-    return {
-      show: false,
-      title: "地址",
-      value: [],
-      showAddress: false,
-      addressData: ChinaAddressV4Data
-    };
-  },
-  mounted() {},
-  methods: {
-    onCancel() {
-      console.log("on cancel");
-    },
-    onConfirm(msg) {
-      console.log("on confirm");
-      if (msg) {
-        alert(msg);
-      }
-    }
-  }
-};
+	components: {
+		Confirm,
+		Picker,
+		XAddress,
+		Group
+	},
+	name: 'personalSetting',
+	data() {
+		return {
+			show: false,
+			title: '地址',
+			value: [],
+			showAddress: false,
+			addressData: ChinaAddressV4Data
+		}
+	},
+	mounted() {},
+	methods: {
+		onCancel() {
+			console.log('on cancel')
+		},
+		onConfirm(msg) {
+			console.log('on confirm')
+			if (msg) {
+				alert(msg)
+			}
+		}
+	}
+}
 </script>
 
