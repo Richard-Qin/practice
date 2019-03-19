e58b15fef57fa414926e63f4906519a172325352
xuweicheng <qin_liyao@163.com>
2019-03-19 16:08:54 +0800

    test

--- src/pages/personal-center/index.vue
+++ src/pages/personal-center/index.vue
@@ -25,7 +25,7 @@
                     </div>
                     <div class="tabbar_flex">
                         <span>卷</span>
-                        <span>9999</span>
+                        <span>9</span>
                     </div>
                 </div>
                 <svg class="icon head_setting" aria-hidden="true" @click="jumpSet()">
